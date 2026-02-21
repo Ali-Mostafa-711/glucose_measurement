f842fd9373bc1c6e8f50ee46fbbdaff95126e3e3
TOSHKA-711 <aliovich711@gmail.com>
2026-02-21 12:22:20 +0200

    last

--- app/pages/home/homeComponents/Products.jsx
+++ app/pages/home/homeComponents/Products.jsx
@@ -3,84 +3,80 @@ import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
 const Products = () => {
+  const products = [
+{
+      img: "https://media.gettyimages.com/id/1432982719/photo/glipizide-pill-bottle-conceptual-image.webp?s=2048x2048&w=gi&k=20&c=GFvBxio4lYCcJ1hAmUF343YeeRaqdj3l9GcE7BmhWQE=",
+      title: "Glipizide",
+      description:
+        "Stimulates the pancreas to release more insulin, used for type 2 diabetes management.",
+      id: "glipizide",
+      category: "medicine",
+      price: 70,
+    },
+    {
+     img: "/med8.jpeg",
+      title: "Victoza",
+      description:
+        "Active Ingredient: Liraglutide. Description: Helps control post-meal blood sugar and supports heart health",
+      id: "liraglutide",
+      category: "medicine",
+      price: 1600,
+    },
+    {
+      img: "https://www.clinicaltrialsarena.com/wp-content/uploads/sites/22/2014/01/Image-1-Forxiga-dapagliflozin-shutterstock_2401535759.jpg",
+      title: "Forxiga",
+      description:
+        "Active Ingredient: Dapagliflozin. Description: Lowers blood sugar by removing excess glucose through urine",
+      id: "dapagliflozin",
+      category: "medicine",
+      price: 600,
+    },
+
+    {
+      img: "/dev2.jpeg",
+      title: "Contour Plus",
+      description:
+        "Simple and easy-to-use blood glucose meter Clear screen and easy buttons  Good accuracy for daily use",
+      id: "Contour",
+      category: "device",
+      price: 800,
+    },
+    {
+      img: "/dev3.jpeg",
+      title: "FreeStyle Libre reader",
+      description:
+        "Part of the FreeStyle Libre system, displays readings on a screen.",
+      id: "FreeStyle",
+      category: "device",
+      price: 800,
+    },
+    {
+      img: "/dev4.jpeg",
+      title: "Continuous Glucose Monitoring (CGM) device",
+      description:
+        "small sensor under the skin readsglucose levels periodically without pricking.",
+      id: "CGM",
+      category: "device",
+      price: 600,
+    },
+  ];
   return (
     <div className="products-page flex flex-col items-center justify-center -mt-25 gap-10 py-40">
       <h1 className="text-[#0643DD] text-5xl font-bold">
         Diabetes Medications
       </h1>
       <div className="product-list grid grid-cols-1 md:grid-cols-3 gap-10">
-        <ProductCard
-          img={
-            "https://media.gettyimages.com/id/526612260/photo/diabetes-medcine-pills.webp?s=2048x2048&w=gi&k=20&c=7_5N85EtRWNvQjj261KSK2JyuszwiIi_3ptygJYMP14="
-          }
-          title={"Metformin"}
-          description={
-            "Helps control blood sugar levels in type 2 diabetes by improving insulin sensitivity."
-          }
-          isDoctor={false}
-          id={"metformin"}
-          price={30}
-        />
-        <ProductCard
-          img={
-            "https://media.gettyimages.com/id/1432982719/photo/glipizide-pill-bottle-conceptual-image.webp?s=2048x2048&w=gi&k=20&c=GFvBxio4lYCcJ1hAmUF343YeeRaqdj3l9GcE7BmhWQE="
-          }
-          title={"Glipizide"}
-          description={
-            "Stimulates the pancreas to release more insulin, used for type 2 diabetes management."
-          }
-          isDoctor={false}
-          id={"glipizide"}
-          price={70}
-        />
-        <ProductCard
-          img={
-            "https://media.gettyimages.com/id/2228094865/photo/portugal-regulates-prescription-of-glp-1-receptor-agonist-class-such-as-ozempic-as-demand.webp?s=2048x2048&w=gi&k=20&c=i0YSF4Rfp1DfpKQvwXZLtrX3OeL_9lbWAoaRo1JM9sU="
-          }
-          title={"Liraglutide (Victoza)"}
-          description={
-            "A GLP-1 receptor agonist that helps lower blood sugar and supports weight loss."
-          }
-          isDoctor={false}
-          id={"liraglutide"}
-          price={150}
-        />
-        <ProductCard
-          img={
-            "https://www.clinicaltrialsarena.com/wp-content/uploads/sites/22/2014/01/Image-1-Forxiga-dapagliflozin-shutterstock_2401535759.jpg"
-          }
-          title={"Dapagliflozin (Forxiga)"}
-          description={
-            "SGLT2 inhibitor that helps the kidneys remove glucose from the bloodstream."
-          }
-          isDoctor={false}
-          id={"dapagliflozin"}
-          price={250}
-        />
-        <ProductCard
-          img={
-            "https://media.gettyimages.com/id/2150938026/photo/human-crowd-surrounding-an-injectable-insulin-bottle-on-purple-background.webp?s=2048x2048&w=gi&k=20&c=vkrLbbXPxtm48P0z7Q09GAhh66kAW4GoRbRiXWX11sc="
-          }
-          title={"Insulin"}
-          description={
-            "Essential for type 1 diabetes and advanced type 2 diabetes to regulate blood glucose."
-          }
-          isDoctor={false}
-          id={"insulin"}
-          price={70}
-        />
-        <ProductCard
-          img={
-            "https://media.gettyimages.com/id/1432982374/photo/sitagliptin-pill-conceptual-image.webp?s=2048x2048&w=gi&k=20&c=BAu5DpylVlz0yE17njLiLCEpiG1rdUrQHTwWNpTL2nY="
-          }
-          title={"Sitagliptin (Januvia)"}
-          description={
-            "DPP-4 inhibitor that helps increase insulin release and decrease glucagon levels."
-          }
-          isDoctor={false}
-          id={"sitagliptin"}
-          price={60}
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            img={product.img}
+            title={product.title}
+            description={product.description}
+            isDoctor={false}
+            id={product.id}
+            price={product.price}
+          />
+        ))}
       </div>
 
       <Link
